a3df793c7cb3b9590a46027b7c2517544e4a96b8
Zbigniew Kostrzewa <zkostrzewa@gmail.com>
2024-03-24 12:53:05 +0100

    Read configuration from config file or environment variables

--- cmd/main.go
+++ cmd/main.go
@@ -6,10 +6,9 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 
+	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/jcmturner/gokrb5/spnego"
 	"github.com/kostrzewa9ld/hadoop-http-jmx-exporter/collectors"
 	"github.com/kostrzewa9ld/hadoop-http-jmx-exporter/httpclient"
@@ -20,43 +19,41 @@ import (
 	"gopkg.in/jcmturner/gokrb5.v7/keytab"
 )
 
-func createHttpClient() httpclient.HttpClient {
-	timeout := 60 * time.Second
-	timeoutEnv := os.Getenv("HTTP_CLIENT_TIMEOUT_SECONDS")
-	if timeoutEnv != "" {
-		i, err := strconv.Atoi(timeoutEnv)
-		if err != nil {
-			log.Fatalf("Failed to parse HTTP_CLIENT_TIMEOUT_SECONDS: %e", err)
-		}
-		timeout = time.Duration(i) * time.Second
-	}
-	noSpnegoClient := http.Client{Timeout: timeout}
+type Config struct {
+	HttpClientTimeoutSeconds time.Duration `yaml:"http_client_timeout_seconds" env:"HTTP_CLIENT_TIMEOUT_SECONDS" env-default:"60s"`
+	KerberosPrincipal        string        `yaml:"kerberos_principal" env:"KERBEROS_PRINCIPAL"`
+	KerberosRealm            string        `yaml:"kerberos_realm" env:"KERBEROS_REALM"`
+	KerberosKeytabPath       string        `yaml:"kerberos_keytab_path" env:"KERBEROS_KEYTAB_PATH"`
+	KerberosConfigPath       string        `yaml:"kerberos_config_path" env:"KERBEROS_CONFIG_PATH"`
+	JmxUrls                  []string      `yaml:"jmx_urls" env:"JMX_URLS" env-required:""`
+	ListenAddress            string        `yaml:"listen_address" env:"LISTEN_ADDRESS" env-default:"0.0.0.0"`
+	ListenPort               int           `yaml:"listen_port" env:"LISTEN_PORT" env-default:"9100"`
+}
 
-	if os.Getenv("KERBEROS_PRINCIPAL") != "" {
-		keytab, err := keytab.Load(os.Getenv("KERBEROS_KEYTAB_PATH"))
+func createHttpClient(cfg *Config) httpclient.HttpClient {
+	noSpnegoClient := http.Client{Timeout: time.Duration(cfg.HttpClientTimeoutSeconds)}
+
+	if cfg.KerberosPrincipal != "" {
+		keytab, err := keytab.Load(cfg.KerberosKeytabPath)
 		if err != nil {
 			log.Fatalf("Failed to load keytab: %e", err)
 		}
 
-		config, err := config.Load(os.Getenv("KERBEROS_CONFIG_PATH"))
+		config, err := config.Load(cfg.KerberosConfigPath)
 		if err != nil {
 			log.Fatalf("Failed to load config: %e", err)
 		}
 
-		ktbClient := client.NewClientWithKeytab(os.Getenv("KERBEROS_PRINCIPAL"), os.Getenv("KERBEROS_REALM"), keytab, config)
+		ktbClient := client.NewClientWithKeytab(cfg.KerberosPrincipal, cfg.KerberosRealm, keytab, config)
 		return httpclient.HttpClientWithSpnego{Client: spnego.NewClient(ktbClient, &noSpnegoClient, "")}
 	}
 
 	return httpclient.HttpClientPure{Client: &noSpnegoClient}
 }
 
-func readJMXUrls() []url.URL {
-	urlsFromEnv := os.Getenv("JMX_URLS")
-	if urlsFromEnv == "" {
-		log.Fatalf("JMX_URLS env variable is not set")
-	}
+func readJMXUrls(cfg *Config) []url.URL {
 	urls := []url.URL{}
-	for _, urlFromEnv := range strings.Split(urlsFromEnv, " ") {
+	for _, urlFromEnv := range cfg.JmxUrls {
 		u, err := url.Parse(urlFromEnv)
 		if err != nil {
 			log.Fatalf("Failed to parse url: %e", err)
@@ -67,25 +64,31 @@ func readJMXUrls() []url.URL {
 }
 
 func main() {
-	collector, err := collectors.NewMetricsCollector(createHttpClient(), readJMXUrls())
+	var cfg Config
+	if len(os.Args) > 1 {
+		err := cleanenv.ReadConfig(os.Args[1], &cfg)
+		if err != nil {
+			log.Fatalf("Failed to read configuration file : %e", err)
+		}
+	} else {
+		log.Println("config.yml not found reading from env variables only")
+		err := cleanenv.ReadEnv(&cfg)
+		if err != nil {
+			log.Fatalf("Failed to read config: %e", err)
+		}
+	}
+
+	collector, err := collectors.NewMetricsCollector(createHttpClient(&cfg), readJMXUrls(&cfg))
 	if err != nil {
 		log.Fatalf("Failed to create collector: %e", err)
 	}
 	registry := prometheus.NewRegistry()
 	registry.Register(collector)
 
-	address := os.Getenv("LISTEN_ADDRESS")
-	if address == "" {
-		address = "0.0.0.0"
-	}
-	port := os.Getenv("LISTEN_PORT")
-	if port == "" {
-		port = "9100"
-	}
-	log.Printf("Listening on %s:%s", address, port)
+	log.Printf("Listening on %s:%s", cfg.ListenAddress, cfg.ListenPort)
 	http.Handle("/health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	}))
 	http.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", cfg.ListenAddress, cfg.ListenPort), nil))
 }
--- go.mod
+++ go.mod
@@ -3,16 +3,20 @@ module github.com/kostrzewa9ld/hadoop-http-jmx-exporter
 go 1.22
 
 require (
+	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/jcmturner/gokrb5 v8.4.4+incompatible
 	github.com/prometheus/client_golang v1.19.0
 	gopkg.in/jcmturner/gokrb5.v7 v7.5.0
 )
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
 	github.com/jcmturner/gofork v1.7.6 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
@@ -24,4 +28,6 @@ require (
 	gopkg.in/jcmturner/dnsutils.v1 v1.0.1 // indirect
 	gopkg.in/jcmturner/goidentity.v3 v3.0.0 // indirect
 	gopkg.in/jcmturner/rpc.v1 v1.1.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
